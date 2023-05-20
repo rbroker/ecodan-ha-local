26bc22217639bd535be63372429c13baa6fd959a
Richard Broker <rstbroker@gmail.com>
2023-05-20 10:14:35 +0100

    Add statistic sensors for z1/z2 room temperatures

--- ecodan-ha-local/ehal_mqtt.cpp
+++ ecodan-ha-local/ehal_mqtt.cpp
@@ -472,6 +472,12 @@ off
         if (!publish_ha_float_sensor_auto_discover(F("dhw_delivered"), SensorType::POWER))
             anyFailed = true;
 
+        if (!publish_ha_float_sensor_auto_discover(F("z1_room_temp"), SensorType::TEMPERATURE))
+            anyFailed = true;
+
+        if (!publish_ha_float_sensor_auto_discover(F("z2_room_temp"), SensorType::TEMPERATURE))
+            anyFailed = true;
+
         if (!anyFailed)
         {
             needsAutoDiscover = false;
@@ -543,6 +549,8 @@ off
         publish_sensor_status<float>(F("heating_delivered"), status.EnergyDeliveredHeating);
         publish_sensor_status<float>(F("dhw_consumed"), status.EnergyConsumedDhw);
         publish_sensor_status<float>(F("dhw_delivered"), status.EnergyDeliveredDhw);
+        publish_sensor_status<float>(F("z1_room_temp"), status.Zone1RoomTemperature);
+        publish_sensor_status<float>(F("z2_room_temp"), status.Zone2RoomTemperature);
     }
 
     bool connect()
